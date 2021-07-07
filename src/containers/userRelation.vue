b65c4a4ff07250837ab8889deecd448a8a864529
wudelai <415587109@qq.com>
2021-07-07 20:25:06 +0800

    公众号相关提交

--- src/containers/userRelation.vue
+++ src/containers/userRelation.vue
@@ -5,7 +5,7 @@
             <van-grid-item icon="add-o" text="添加绑定" to="/addUserRelation" />
             <van-grid-item icon="close" text="取消绑定" to="/deleteUserRelation" />
         </van-grid>
-        <van-notice-bar  background="#ecf9ff" left-icon="info-o" wrapable="true">
+        <van-notice-bar  background="#ecf9ff" left-icon="info-o" wrapable>
             一个微信号只能绑定一个理赔账号，如需变更，先取消绑定再添加
         </van-notice-bar>
     </section>
